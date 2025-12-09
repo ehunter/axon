b9dddddc6fc22d777de9fdf5f864087603924cda
ehunter <erik.maxwell.hunter@gmail.com>
2025-12-09 10:44:16 -0500

    fix: Remove top padding from dashboard main area

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
       <Sidebar />
 
       {/* Main content area */}
-      <main className="flex-1 flex flex-col overflow-hidden p-4 pl-0">
+      <main className="flex-1 flex flex-col overflow-hidden pr-4 pb-4">
         {children}
       </main>
     </div>
