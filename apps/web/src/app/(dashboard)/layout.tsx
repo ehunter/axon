5ccfec9b811b009e0243a9a3aeb3f754092f3349
ehunter <erik.maxwell.hunter@gmail.com>
2025-12-11 12:40:23 -0500

    Remove padding-right and padding-bottom from main container

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
       <Sidebar />
 
       {/* Main content area */}
-      <main className="flex-1 flex flex-col overflow-hidden pr-4 pb-4">
+      <main className="flex-1 flex flex-col overflow-hidden">
         {children}
       </main>
     </div>
